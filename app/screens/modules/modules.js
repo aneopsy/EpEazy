590611f597e27ef00d8b9e91724ae9b2bd3a0834
Blanchard-A <alexandre.blanchard@epitech.eu>
2018-11-07 18:24:18 +0100

    Filter

--- app/screens/modules/modules.js
+++ app/screens/modules/modules.js
@@ -1,12 +1,35 @@
 import React from "react";
 import { FlatList, View, Image, Dimensions } from "react-native";
 import { connect } from "react-redux";
-import { RkStyleSheet, RkText } from "react-native-ui-kitten";
+import { RkStyleSheet, RkText, RkChoice } from "react-native-ui-kitten";
 import { TabView, TabBar, SceneMap } from "react-native-tab-view";
 
 import { getModules, getActivities } from "../../redux/modules/action";
+import { RkSwitch } from "react-native-ui-kitten";
 
 const moment = require("moment");
+
+class Filter extends React.Component {
+  render = () => (
+    <View style={styles.viewRow}>
+      <View style={[styles.viewCenterMargin, styles.viewRow]}>
+        <RkText rkType="primary2">Registered</RkText>
+        <RkSwitch
+          value={this.props.registeredFilter}
+          onValueChange={this.props.onFilterChanged}
+          style={styles.switchTransform}
+        />
+      </View>
+      <View style={[styles.viewCenterMargin, styles.viewRow]}>
+        <RkText rkType="primary2">Sort:</RkText>
+        <RkChoice rkType="radio" style={{ margin: 8 }} />
+        <RkText rkType="secondary4">A - Z</RkText>
+        <RkChoice rkType="radio" style={{ margin: 8 }} />
+        <RkText rkType="secondary4">Date</RkText>
+      </View>
+    </View>
+  );
+}
 
 class Modules extends React.Component {
   static navigationOptions = {
@@ -18,7 +41,8 @@ class Modules extends React.Component {
     routes: [
       { key: "modules", title: "Modules" },
       { key: "activities", title: "Activities" }
-    ]
+    ],
+    registeredFilter: false
   };
 
   componentDidMount() {
@@ -47,7 +71,7 @@ class Modules extends React.Component {
       <View style={styles.content}>
         <View style={styles.mainContent}>
           <View style={styles.text}>
-              <RkText rkType="header6">{item.acti_title}</RkText>
+            <RkText rkType="header6">{item.acti_title}</RkText>
           </View>
         </View>
         {this.renderAttachment(item)}
@@ -65,7 +89,16 @@ class Modules extends React.Component {
       <View style={styles.content}>
         <View style={styles.mainContent}>
           <View style={styles.text}>
-              <RkText rkType="header6">{item.title}</RkText>
+            <RkText
+              rkType="header6"
+              style={
+                item.status === "notregistered"
+                  ? { color: "#888" }
+                  : { color: "#000" }
+              }
+            >
+              {item.title}
+            </RkText>
           </View>
         </View>
         {this.renderAttachment(item)}
@@ -74,7 +107,7 @@ class Modules extends React.Component {
   );
 
   modulesRoute = () =>
-    (this.props.modules.length !== 0) ? (
+    this.props.modules.length !== 0 ? (
       <FlatList
         style={styles.root}
         data={this.props.modules}
@@ -83,24 +116,48 @@ class Modules extends React.Component {
       />
     ) : (
       <View style={styles.textContent}>
-        <RkText rkType="light" style={styles.paragraph}>No message</RkText>
+        <RkText rkType="light" style={styles.paragraph}>
+          No Modules
+        </RkText>
       </View>
     );
 
-  activitiesRoute = () => 
-    (this.props.activities.length !== 0) ? (
-      <FlatList
-        style={styles.root}
-        data={this.props.activities}
-        renderItem={this.renderActivitiesItem}
-        keyExtractor={this.extractItemKey}
+  activitiesRoute = () => (
+    <View>
+      <Filter
+        registeredFilter={this.state.registeredFilter}
+        onFilterChanged={this.onFilterChanged}
       />
-    ) : (
-      <View style={styles.textContent}>
-        <RkText rkType="light" style={styles.paragraph}>No Alert</RkText>
+      {this.props.activities.length !== 0 ? (
+      <View>
+        <FlatList
+          style={styles.root}
+          data={
+            this.state.registeredFilter
+              ? this.props.activities.filter(item => item.status === "ongoing")
+              : this.props.activities
+          }
+          renderItem={this.renderActivitiesItem}
+          keyExtractor={this.extractItemKey}
+        />
       </View>
-    );
+      ) : (
+      <View style={styles.textContent}>
+        <RkText rkType="light" style={styles.paragraph}>
+          No Activities
+        </RkText>
+      </View>
+      )}
+    </View>
+  );
 
+  onFilterChanged = value => {
+      console.log(this.state.registeredFilter)
+        this.setState({
+      registeredFilter: value
+    });
+    console.log(this.state.registeredFilter)
+  };
 
   render() {
     return (
@@ -115,6 +172,7 @@ class Modules extends React.Component {
           width: Dimensions.get("window").width,
           height: Dimensions.get("window").height
         }}
+        tabBarPosition="bottom"
       />
     );
   }
@@ -122,9 +180,9 @@ class Modules extends React.Component {
 
 const styles = RkStyleSheet.create(theme => ({
   paragraph: {
-    textAlign: 'center',
-    alignSelf: 'center',
-    justifyContent: 'center',
+    textAlign: "center",
+    alignSelf: "center",
+    justifyContent: "center"
   },
   root: {
     backgroundColor: theme.colors.screen.base
@@ -134,7 +192,7 @@ const styles = RkStyleSheet.create(theme => ({
     flexDirection: "row",
     borderBottomWidth: 1,
     borderColor: theme.colors.border.base,
-    justifyContent: 'flex-start',
+    justifyContent: "flex-start"
   },
   avatar: {
     alignSelf: "center"
@@ -150,9 +208,9 @@ const styles = RkStyleSheet.create(theme => ({
   mainContent: {},
   textContent: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
-    padding: 8,
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1",
+    padding: 8
   },
   img: {
     height: 50,
@@ -162,19 +220,34 @@ const styles = RkStyleSheet.create(theme => ({
   attachment: {
     position: "absolute",
     right: 0
+  },
+  viewCenterMargin: {
+    margin: 8,
+    alignItems: "center"
+  },
+  viewCenter: {
+    alignItems: "center"
+  },
+  viewRow: {
+    backgroundColor: "white",
+    flexDirection: "row"
+  },
+  switchTransform: {
+    margin: 8,
+    transform: [{ scaleX: 0.6 }, { scaleY: 0.6 }]
   }
 }));
 
 const mapStateToProps = state => {
   return {
     modules: state.Modules.modules,
-    activities: state.Modules.activities.items,
+    activities: state.Modules.activities.items
   };
 };
 
 const mapDispatchToProps = {
   getModules,
-  getActivities,
+  getActivities
 };
 
 export default connect(
